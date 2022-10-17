6e2076bed4ec8e9951280a27802a8e584767968b
atamswgo <atamswgo1@gmail.com>
2022-10-17 15:20:14 +0100

    adding Music background Image

--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<center>Hello, world. You're at the Music app index.<b><br/> <h2>Congratulations on successfull creation of first adjango App</h2> </center>")
+    return HttpResponse(" <center>Hello, world. You're at the Music app index.<b><br/> <h2>Congratulations on successfull creation of first adjango App</h2> <img src='https://st2.depositphotos.com/2075965/6365/v/600/depositphotos_63653511-stock-illustration-abstract-colorful-music-notes.jpg' alt ='Music background'<br/> <strong> <h4>Enjoy your Music on the Go!</h4></strong></center>")
