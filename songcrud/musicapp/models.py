200787fbb3403af7825bfba48b86fae2829148eb
WonahGodwino <atamswgo1@gmail.com>
2022-10-25 18:14:36 +0100

    Editing table fields

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,17 +1,21 @@
+from asyncio.windows_events import NULL
+from email.policy import default
 from enum import unique
+from pickle import TRUE
 from pyexpat import model
 from django.db import models
 
 # Create your models here.
 class Artiste(models.Model):
-    artiste_id = models.CharField(max_length=10, primary_key=True)
+    artiste_id = models.CharField(max_length=25,primary_key=True, serialize=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    age = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    
 
 class Song(models.Model):
     title = models.CharField(max_length=100)
-    release_date = models.DateField()
+    release_date = models.DateField() 
     likes = models.DateField(max_length=50)
     artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
 
